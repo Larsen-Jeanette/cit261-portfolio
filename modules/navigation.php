2bbc9462e2a4a8f5874468c0213be299189df744
Larsen-Jeanette <jnet.larsen@gmail.com>
2016-02-20 22:58:38 -0500

    Updated navigation

--- modules/navigation.php
+++ modules/navigation.php
@@ -1,7 +1,6 @@
  <div id="navigation">
     <h2>Topics</h2>
     <ol>
-    	<li><a href="/topics/mobile-design.php">Creative, pleasant, mobile user interface creation</a></li>
       <li><a href="/topics/1.php">Loops, Conditional Statements, Functions, Variables, Parameters, Arrays, Associative Arrays</a></li>
     	<li><a href="/topics/2.php">Java Script Object Creation, Properties, Methods, Instantiation</a></li>
       <li><a href="/topics/3.php">JSON Parse, Stringify</a></li>
@@ -11,8 +10,10 @@
     	<li><a href="/topics/7.php">Manipulating CSS Class Properties Using JavaScript</a></li>
       <li><a href="/topics/8.php">Using AJAX - to Consume a JSON Web Service</a></li>
       <li><a href="/topics/9.php">Standard JavaScript Events Including those for Mobile Devices ( Ex. onTouchBegin, onLoad, etc.)</a></li>
-        
       <li><a href="/topics/10.php">HTML5 Tags - Video, Audio, Camera, Canvas, etc.</a></li>
     	<li><a href="/topics/11.php">Mobile Single Page App Design and Implementation</a></li>
     </ol>
+    <ul>
+      <li><a href="/topics/mobile-design.php">Creative, pleasant, mobile user interface creation</a></li>
+    </ul>
 </div>
