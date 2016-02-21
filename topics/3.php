a958437f93415f7ff78886694a631d1d7d43cda2
Larsen-Jeanette <jnet.larsen@gmail.com>
2016-02-21 00:09:12 -0500

    JSON beginning notes and example.

--- topics/3.php
+++ topics/3.php
@@ -13,7 +13,43 @@
     
   	<h1>3. JSON Parse, Stringify</h1>
 
- 	 <p>Content coming soon.</p>
+ 	 <p>JSON is JavaScript Object Notation, and it is basically an easy way to store and share data that is easier than XML.</p>
+
+   <p>***Don't store items that are sensitive with JSON!</p>
+
+
+
+   <h3>Compare examples of JSON to XML</h3>
+
+   <p>JSON</p>
+     <p>{"employees":[
+        <p>{"firstName":"John", "lastName":"Doe"},</p>
+        <p>{"firstName":"Anna", "lastName":"Smith"},</p>
+        <p>{"firstName":"Peter", "lastName":"Jones"}</p>
+      <p>]}</p>
+
+  <p>XML</p>
+    <p>(defines an object with 3 items)</p>
+
+    <img src="/media/xml-example.png" alt="xml example"></a>
+
+
+   <h3></h3>
+   <p></p>
+   <script></script>
+
+   <h3></h3>
+   <p></p>
+   <script></script>
+
+
+   <h3></h3>
+   <p></p>
+   <script></script>
+
+   <h3></h3>
+   <p></p>
+   <script></script>
 
   </main>
   </body>
