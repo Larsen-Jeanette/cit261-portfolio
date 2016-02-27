deebf7bfc30d0f362250163e3cd7c05f010c48f9
Larsen-Jeanette <jnet.larsen@gmail.com>
2016-02-27 12:06:56 -0500

    Local storage using json stringify

--- topics/4.php
+++ topics/4.php
@@ -9,12 +9,39 @@
       <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/navigation.php';  ?>   
     </header>
 
-  <main> 
+    <main> 
 
-    <h1>4. Local Storage API, Storing and Retrieving Simple Data, Arrays, Associative Arrays, and Objects</h1>
+     <h1>4. Local Storage API, Storing and Retrieving Simple Data, Arrays, Associative Arrays, and Objects</h1>
 
- 	  <p>Content coming soon.</p>
+    <div id="date"></div>
+    <div id="monthList"></div>
 
-  </main>
+      <script>
+        //javascript object
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        
+        // Check browser support
+        if (typeof(Storage) !== "undefined") {
+
+          // Takes JavaScript object, turns it into a JSON string. Storing it locally.
+          localStorage.setItem("calendarMonths", JSON.stringify(months));
+            
+          // Retrieving the JSON string from the local storage and returning it as an array
+          document.getElementById("date").innerHTML = localStorage.getItem("calendarMonths");
+
+          } 
+        
+        else {
+          document.getElementById("date").innerHTML = "Sorry, your browser does not support Web Storage...";
+        }
+        
+        //taking stored JSON string and is parsing back into a regular string
+        var Retrieve = localStorage.getItem("calendarMonths");
+        var calMon = JSON.parse(Retrieve);
+        
+        document.getElementById("monthList").innerHTML = calMon;
+
+      </script>
+    </main>
   </body>
 </html>
